b24f42ba0fea595680cc0df75b2ddae5b4dc3975
PaperAndColours <BenPersonal@Birminghamguitartuition.co.uk>
2014-11-07 10:12:43 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+(function() {
+  var app = angular.module('doList', []);
+
+  app.controller('DoListController', [ '$http', function($http) {
+   this.curList = 0;
+   var dolist = this;
+   dolist.actions = [];
+   dolist.contexts = [];
+   dolist.projects = [];
+   dolist.productOrder = "name";
+   $http.get('/dolist.json').success(function(data){
+     dolist.actions  = data.actions;
+     dolist.contexts  = data.contexts;
+     dolist.projects  = data.projects;
+    });
+   this.sortProducts = function(category) {
+    store.productOrder=category;
+  };
+  this.selectList = function(list){
+    this.curList = list;
+  };
+  this.listSelected = function(list) {
+    return this.curList === list;
+  };
+  }]);
+})();
+
