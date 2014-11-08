95ee15a402c8eecb4f38a10e33d061a79a31eef3
PaperAndColours <BenPersonal@Birminghamguitartuition.co.uk>
2014-11-08 09:44:47 +0000

    Date stamped + actions sortable
    All actions have date stamps
    
    All actions are sortable by any parameter, and in reverse order

--- app.js
+++ app.js
@@ -8,15 +8,41 @@
    dolist.contexts = [];
    dolist.projects = [];
    dolist.newAction = {};
-   dolist.productOrder = "name";
+
+   dolist.actionOrderReverse = false
+   //Tracks whether ascending or descending order last used for each action
+   dolist.actionOrderReverseHistory = {};
+    
    $http.get('/dolist.json').success(function(data){
      dolist.actions  = data.actions;
      dolist.contexts  = data.contexts;
      dolist.projects  = data.projects;
     });
-   this.sortProducts = function(category) {
-    store.productOrder=category;
+
+   this.sortActionsBy = function(category) {
+    if (dolist.actionOrderReverseHistory[category] === undefined) {
+       this.actionOrderReverseHistory[category] = false;
+        }
+    else if (this.actionOrder == category){
+        this.actionOrderReverseHistory[category] = !this.actionOrderReverseHistory[category];
+        }
+    this.actionOrderReverse = this.actionOrderReverseHistory[category];
+    this.actionOrder=category;
   };
+
+
+   dolist.sortActionsBy('dateAdded');
+   dolist.sortActionsBy('dateAdded'); //doubled to reverse order, newest first
+
+    this.sortDirectionToChar = function(category){
+        if (this.actionOrderReverseHistory[category] === undefined){
+            return "";}
+        if (this.actionOrderReverseHistory[category] === false){
+            return "(\u25B2)";}
+        if (this.actionOrderReverseHistory[category] === true){
+            return "(\u25BC)";}
+    };
+
   this.selectList = function(list){
     this.curList = list;
   };
@@ -24,6 +50,7 @@
     return this.curList === list;
   };
   this.addAction = function(list) {
+    dolist.newAction.dateAdded = Date.now();
     dolist.actions.push(dolist.newAction);
     dolist.newAction = {};
   }
