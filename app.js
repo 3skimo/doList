758aa5f5f7b0f0b774b1ebe07ea4c513910cbf9e
PaperAndColours <BenPersonal@Birminghamguitartuition.co.uk>
2014-11-12 11:10:37 +0000

    Added display for collated based items

--- app.js
+++ app.js
@@ -16,18 +16,17 @@
 
 
    dolist.sort_by = function(field, reverse, primer){
-
      var key = primer ? 
          function(x) {return primer(x[field])} : 
          function(x) {return x[field]};
-
      reverse = [-1, 1][+!!reverse];
-
      return function (a, b) {
          return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
        } 
   };
-
+  dolist.setHeader = function(headerName) {
+    
+    }
         
 
    this.sortActionsBy = function(category) {
@@ -54,9 +53,7 @@
           }
           else
               sortableEntries[theHeader] = [theAction]; 
-
       }
-
       return  sortableEntries;
     }
 
@@ -65,6 +62,11 @@
      dolist.contexts  = data.contexts;
      dolist.projects  = data.projects;
      dolist.collatedActions = dolist.updateCollatedEntries(dolist.currentCollateProperty);
+     dolist.collatedHeaders = [];
+     for (var key in dolist.collatedActions) {
+         if (key != undefined)
+         dolist.collatedHeaders.push(key);
+     }
     });
 
 
