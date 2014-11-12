939605348fafe444e62828dacc5bc1a75e0de8a1
Ben Mann <ben@birminghamguitartuition.com>
2014-11-12 23:40:40 +0000

    Tidied code, seperated into sections for reability

--- app.js
+++ app.js
@@ -15,8 +15,8 @@
   
    dolist.currentCollateProperty = "context"
 
-
-   dolist.sort_by = function(field, reverse, primer){
+//---------------Utility Functions-------------
+   this.sort_by = function(field, reverse, primer){
      var key = primer ? 
          function(x) {return primer(x[field])} : 
          function(x) {return x[field]};
@@ -26,7 +26,20 @@
        } 
   };
         
+  this.keepOnlyDayMonthYearInTimestamp = function(timestamp) {
+    date = moment(timestamp);
+    date.milliseconds(0)
+    date.seconds(0)
+    date.minutes(0)
+    date.hours(0)
+    simplifiedTimestamp = date.unix() * 1000;
+    return simplifiedTimestamp;
+  };
 
+
+
+//---------------Sorting functions -------------
+//sort actions (within categories... (but this isn't fully integrated yet!))
    this.sortActionsBy = function(category) {
     if (dolist.actionOrderReverseHistory[category] === undefined) {
        this.actionOrderReverseHistory[category] = false;
@@ -38,7 +51,16 @@
     this.actionOrder=category;
   };
 
+    this.sortDirectionToChar = function(category){
+        if (this.actionOrderReverseHistory[category] === undefined){
+            return "";}
+        if (this.actionOrderReverseHistory[category] === false){
+            return "(\u25B2)";}
+        if (this.actionOrderReverseHistory[category] === true){
+            return "(\u25BC)";}
+    };
 
+//---------------Collated headers-------------
    dolist.updateCollatedEntries = function(collateProperty) {
       sortedList = dolist.actions.sort(dolist.sort_by(collateProperty, false, function(a){return a}));
       sortableEntries = {};
@@ -63,13 +85,6 @@
      }
      return collatedHeaders;
 }
-   $http.get('/dolist.json').success(function(data){
-     dolist.actions  = data.actions;
-     dolist.contexts  = data.contexts;
-     dolist.projects  = data.projects;
-     dolist.collatedActions = dolist.updateCollatedEntries(dolist.currentCollateProperty);
-     dolist.collatedHeaders = dolist.updateCollatedHeaders();
-    });
 
    dolist.setHeader = function(headerName) {
       dolist.currentCollateProperty = headerName;
@@ -77,8 +92,6 @@
       dolist.collatedHeaders = dolist.updateCollatedHeaders();
     }
 
-   dolist.sortActionsBy('dateAdded');
-   dolist.sortActionsBy('dateAdded'); //doubled to reverse order, newest first
 
 
   dolist.displayActionHeader = function(actionHeader) {
@@ -89,39 +102,35 @@
     return actionHeader;
     }
 
-   //dolist.sortedList = dolist.actions; //.sort(dolist.sort_by(dolist.currentSortable, false, function(a){return a.toUpperCase}));
 
-    this.sortDirectionToChar = function(category){
-        if (this.actionOrderReverseHistory[category] === undefined){
-            return "";}
-        if (this.actionOrderReverseHistory[category] === false){
-            return "(\u25B2)";}
-        if (this.actionOrderReverseHistory[category] === true){
-            return "(\u25BC)";}
-    };
-
+//---------------Obsolete? -------------
   this.selectList = function(list){
     this.curList = list;
   };
   this.listSelected = function(list) {
     return this.curList === list;
   };
+
+//---------------Basic Functionality  -------------
+
   this.addAction = function(list) {
     dolist.newAction.dateAdded = this.keepOnlyDayMonthYearInTimestamp(Date.now());
     dolist.actions.push(dolist.newAction);
     dolist.newAction = {};
     dolist.setHeader(dolist.currentCollateProperty);
   };
+//---------------Actions & callback -------------
+   this.sortActionsBy('dateAdded');
+   this.sortActionsBy('dateAdded'); //doubled to reverse order, newest first
 
-  this.keepOnlyDayMonthYearInTimestamp = function(timestamp) {
-    date = moment(timestamp);
-    date.milliseconds(0)
-    date.seconds(0)
-    date.minutes(0)
-    date.hours(0)
-    simplifiedTimestamp = date.unix() * 1000;
-    return simplifiedTimestamp;
-  };
+   $http.get('/dolist.json').success(function(data){
+     dolist.actions  = data.actions;
+     dolist.contexts  = data.contexts;
+     dolist.projects  = data.projects;
+     dolist.collatedActions = dolist.updateCollatedEntries(dolist.currentCollateProperty);
+     dolist.collatedHeaders = dolist.updateCollatedHeaders();
+    });
+
   }]);
 
 })();
